e9765288db1fba51cce9955ef7272790eb1de648
capik <oitrp345@gmail.com>
2026-05-19 01:06:36 +0300

    fix: correct code style (spaces instead of tabs)

--- calculator.py
+++ calculator.py
@@ -1,28 +1,23 @@
-# VERSION 1
-
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def substract(a, b):
     return a - b
 
-def multiple(a,b):
-    return a*b
+def multiply(a, b):
+    return a * b
 
-def divide (a,b):
-    if b != 0:
-        return a//b
-    else:
-        return "Error"
-    
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
 def main():
     print("Simple calculator")
     print(f"2 + 3 = {add(2, 3)}")
-    print(f"5 - 2 = {subtract(5, 2)}")
-    print(f"5 * 2 = {multiple(5,2)}")
-    print(f"6 : 2 = {divide(6,2)}")
-    print(f"5 : 0 = {divide(5,0)}")
-
+    print(f"5 - 3 = {substract(5, 3)}")
+    print(f"2 * 3 = {multiply(2, 3)}")
+    print(f"6 + 3 = {divide(6, 3)}")
 
 if __name__ == "__main__":
     main()
