816c841b115f1c218214f5484abdcac3ce4fabf0
capik <oitrp345@gmail.com>
2026-05-19 01:09:17 +0300

    finish

--- calculator.py
+++ calculator.py
@@ -1,16 +1,20 @@
 def add(a, b):
     return a + b
 
+
 def substract(a, b):
     return a - b
 
+
 def multiply(a, b):
     return a * b
+
 
 def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
 
 def main():
     print("Simple calculator")
@@ -18,6 +22,7 @@ def main():
     print(f"5 - 3 = {substract(5, 3)}")
     print(f"2 * 3 = {multiply(2, 3)}")
     print(f"6 + 3 = {divide(6, 3)}")
+
 
 if __name__ == "__main__":
     main()
